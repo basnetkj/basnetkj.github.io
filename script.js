e96ff523d6f57b186604514fa429e2ffbdc1c4ae
basnetkj <96842778+basnetkj@users.noreply.github.com>
2024-02-08 17:57:34 -0700

    Major redesign

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar'); // Adjust if your navbar has a different selector
+    if (window.scrollY > 50) { // Check if the page is scrolled more than 50 pixels
+      navbar.classList.add('navbar-scrolled');
+    } else {
+      navbar.classList.remove('navbar-scrolled');
+    }
+  });
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fill');
+      } else {
+        entry.target.classList.remove('fill');
+        entry.target.style.width = '0'; 
+      }
+    });
+  }, {
+    threshold: 0.5 
+  });
+
+  document.querySelectorAll('.skill-per').forEach(skill => {
+    observer.observe(skill);
+  });
+});
+
